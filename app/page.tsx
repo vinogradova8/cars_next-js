4ab8cda9513635843fcd2fd83db82be1fec8feba
Viktoriia Vynohradova <viktoriia.vynohradova8@gmail.com>
2025-04-14 22:51:20 +0300

    created hero component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
-import Image from 'next/image';
+import Hero from '@/components/Hero';
 
 export default function Home() {
   return (
     <div className='grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]'>
-      <main className='overflow-hidden'></main>
+			<main className='overflow-hidden'>
+				<Hero />
+			</main>
     </div>
   );
 }
---
+++ components/Hero.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Hero = () => {
+	return (
+    <div className='hero'>
+      <div className='flex-1 pt-36 padding-x'>
+        <h1 className='hero__title'>
+          Find, book, rent a car — quick and super easy!
+        </h1>
+      </div>
+    </div>
+  );
+}
+
+export default Hero
