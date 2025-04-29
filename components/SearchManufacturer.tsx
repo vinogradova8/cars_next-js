5a31f81083e022ef7f67aced8c9cdf1932d69c57
Viktoriia Vynohradova <viktoriia.vynohradova8@gmail.com>
2025-04-29 17:30:08 +0300

    added headlessui

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -1,11 +1,73 @@
+'use client';
+import {
+  Combobox,
+  ComboboxButton,
+  ComboboxInput,
+  ComboboxOption,
+  ComboboxOptions,
+  Transition,
+} from '@headlessui/react';
 import { SearchManufacturerProps } from '@/types';
-import React from 'react';
+import Image from 'next/image';
+import React, { Fragment, useState } from 'react';
+import { manufacturers } from '@/constants';
 
 const SearchManufacturer = ({
   manufacturer,
   setManufacturer,
 }: SearchManufacturerProps) => {
-  return <div>SearchManufacturer</div>;
+  const [query, setQuery] = useState('');
+
+  const filteredManufacturers =
+    query === ''
+      ? manufacturers
+      : manufacturers.filter((item) =>
+          item.toLowerCase().includes(query.toLowerCase().trim())
+        );
+
+  return (
+    <div className='search-manufacturer'>
+      <Combobox>
+        <div className='relative w-full'>
+          <ComboboxButton className='absolute top-[14px]'>
+            <Image
+              src='/car-logo.svg'
+              width={20}
+              height={20}
+              className='ml-4'
+              alt='Car logo'
+            />
+          </ComboboxButton>
+
+          <ComboboxInput
+            className='search-manufacturer__input'
+            placeholder='Volkswagen'
+            displayValue={(manufacturer: string) => manufacturer}
+            onChange={(e) => setQuery(e.target.value)}
+          />
+
+          <Transition
+            as={Fragment}
+            leave='transition ease-in duration-100'
+            leaveFrom='opacity-100'
+            leaveTo='opacity-0'
+            afterLeave={() => setQuery('')}
+          >
+            <ComboboxOptions>
+              {filteredManufacturers.length === 0 && query !== '' && (
+                <ComboboxOption
+                  value={query}
+                  className='search-manufacturer__option'
+                >
+                  Create "{query}"
+                </ComboboxOption>
+              )}
+            </ComboboxOptions>
+          </Transition>
+        </div>
+      </Combobox>
+    </div>
+  );
 };
 
 export default SearchManufacturer;
